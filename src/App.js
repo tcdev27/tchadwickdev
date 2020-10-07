8b66440d52413c1aa0d23e12d4121e9064c2a50d
tcdev27 <tom.chadwick27@gmail.com>
2020-10-07 17:28:47 +0100

    Updated title and added graphic.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ export default function App() {
   return (
     <div className="App">
       <h1>www.tchadwickdev.co.uk</h1>
+      <img
+        style={{ height: 300, width: 300, marginTop: 150 }}
+        src="img/mechanic.png"
+        alt="Coming soon"
+      />
     </div>
   );
 }
