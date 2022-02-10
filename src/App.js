323c0c2aab130cb654bcf123f28e082ffc875afe
tcdev27 <tom.chadwick27@gmail.com>
2022-02-10 20:35:22 +0000

    Updated image and added p.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './styles.css';
 
 export default function App() {
   return (
-    <div className="App" data-test="true">
+    <div className="App">
       <h1>www.tchadwickdev.co.uk</h1>
       <img
         style={{ height: 300, width: 300, marginTop: 150 }}
         src="img/mechanic.png"
         alt="Coming soon"
       />
+      <p>Let's play a game</p>
     </div>
   );
 }
